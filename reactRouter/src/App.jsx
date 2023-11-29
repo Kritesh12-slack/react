990834daa37cfe5f6862be65a22d8d3b9722e26c
Kritesh12-slack <148207048+Kritesh12-slack@users.noreply.github.com>
2023-11-29 13:00:24 +0000

    changes++

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,10 +1,14 @@
-
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 function App() {
 
   return (
     <>
-        <h1 className="text-3xl text-center bg-slate-700 text-orange-600 p-4">Test</h1>
+      <Header/>
+      <Outlet/>
+      <Footer/>  
     </>
   )
 } 
